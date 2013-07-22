930b54abd8f0918ce2fe74b1eaf1610da3314cd2
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2013-07-22 18:11:44 +0000

    Remove a silly bit of comment formatting

--- nexrad/types.h
+++ nexrad/types.h
@@ -2,7 +2,7 @@
 #define _NEXRAD_TYPES_H
 
 typedef struct _nexrad_date {
-    uint16_t epoch_day;  /* Days since Unix epoch      */
+    uint16_t epoch_day;  /* Days since Unix epoch */
     uint32_t gmt_second; /* Seconds since midnight GMT */
 } nexrad_date;
 
