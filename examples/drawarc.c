1af9c930935cedeb73f15fd289ebc22b05e9915c
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2013-09-27 00:41:33 -0500

    Revert "Remove examples/drawarc.c"
    This reverts commit bbb14b8676a1076ea6cb05b27e4ed5b8023d28b0.

---
+++ examples/drawarc.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <nexrad/image.h>
+
+static void usage(int argc, char **argv) {
+    fprintf(stderr, "usage: %s outfile.png\n", argv[0]);
+    exit(1);
+}
+
+int main(int argc, char **argv) {
+    nexrad_image *image;
+    char *path;
+
+    if (argc != 2) {
+        usage(argc, argv);
+    }
+
+    path = argv[1];
+
+    if ((image = nexrad_image_create(360, 360, NEXRAD_IMAGE_8BPP, NEXRAD_IMAGE_GRAYSCALE)) == NULL) {
+        goto error_image_create;
+    }
+
+    nexrad_image_draw_arc_segment(image, 0x7f, 0, 45, 16, 32);
+
+    //nexrad_image_draw_line(image, 0x7f, 180, 180, 7, 13);
+
+    if (nexrad_image_save_png(image, path) < 0) {
+        goto error_image_save_png;
+    }
+
+    nexrad_image_destroy(image);
+
+    return 0;
+
+error_image_save_png:
+    nexrad_image_destroy(image);
+
+error_image_create:
+    perror("nexrad_image_create()");
+
+    return 1;
+}
