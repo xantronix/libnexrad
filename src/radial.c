6d9d97c0a2cb42193f063f0163de220bad85ed36
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2013-10-23 17:18:58 -0500

    Prevent drawing black pixels

--- src/radial.c
+++ src/radial.c
@@ -430,10 +430,15 @@ static int _image_unpack_rle(nexrad_image *image, nexrad_radial *radial, nexrad_
 
         for (r=0; r<runs; r++) {
             uint8_t level = data[r].level * NEXRAD_RADIAL_RLE_FACTOR;
+            uint8_t r, g, b;
 
-            if (level > 0) {
+            r = entries[level].r;
+            g = entries[level].g;
+            b = entries[level].b;
+
+            if (r || g || b) {
                 nexrad_image_draw_arc_segment(image,
-                    entries[level].r, entries[level].g, entries[level].b,
+                    r, g, b,
                     angle_start, angle_end,
                     radius, radius + data[r].length
                 );
@@ -468,10 +473,15 @@ static int _image_unpack_digital(nexrad_image *image, nexrad_radial *radial, nex
 
         for (b=0; b<bins; b++) {
             uint8_t level = ((uint8_t *)data)[b];
+            uint8_t r, g, b;
 
-            if (level > 0x0f) {
+            r = entries[level].r;
+            g = entries[level].g;
+            b = entries[level].b;
+
+            if (r || g || b) {
                 nexrad_image_draw_arc_segment(image,
-                    entries[level].r, entries[level].g, entries[level].b,
+                    r, g, b,
                     angle_start, angle_end,
                     radius, radius+1
                 );
