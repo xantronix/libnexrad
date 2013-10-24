c26cb0c9568cf1184e41d86031a69297ffee6857
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2013-10-23 22:25:57 -0500

    Update examples/savepng.c

--- examples/savepng.c
+++ examples/savepng.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <nexrad/message.h>
-#include <nexrad/radial.h>
 #include <nexrad/raster.h>
+#include <nexrad/radial.h>
 
 static void usage(int argc, char **argv) {
     fprintf(stderr, "usage: %s colors.clut input.l3 output.png\n", argv[0]);
@@ -15,8 +16,19 @@ static nexrad_image *get_product_image(const char *file, nexrad_color_table *tab
     nexrad_symbology_block *symbology;
     nexrad_chunk *block, *layer;
 
+    nexrad_geo_spheroid *spheroid;
+    nexrad_geo_cartesian radar;
+
     if ((message = nexrad_message_open(file)) == NULL) {
         goto error_message_open;
+    }
+
+    if ((spheroid = nexrad_geo_spheroid_create()) == NULL) {
+        goto error_geo_spheroid_create;
+    }
+
+    if (nexrad_message_read_station_location(message, &radar.lat, &radar.lon, NULL) < 0) {
+        goto error_message_read_station_location;
     }
 
     if ((symbology = nexrad_message_get_symbology_block(message)) == NULL) {
@@ -35,13 +47,19 @@ static nexrad_image *get_product_image(const char *file, nexrad_color_table *tab
             enum nexrad_packet_type type = nexrad_packet_get_type(packet);
 
             switch (type) {
-                case NEXRAD_PACKET_RADIAL:
-                case NEXRAD_PACKET_RADIAL_AF1F: {
+                case NEXRAD_PACKET_RADIAL: {
                     nexrad_radial *radial = nexrad_radial_packet_open((nexrad_radial_packet *)packet);
 
-                    nexrad_image *image = nexrad_radial_create_image(radial, table);
+                    nexrad_image *image = nexrad_radial_create_unprojected_image(radial, table, &radar, spheroid, 0.00815);
 
-                    fprintf(stderr, "Found radial packet, creating image\n");
+                    return image;
+                }
+
+                case NEXRAD_PACKET_RADIAL_AF1F: {
+                    nexrad_radial_packet *digital = nexrad_radial_packet_unpack((nexrad_radial_packet *)packet, NULL);
+                    nexrad_radial *radial = nexrad_radial_packet_open(digital);
+
+                    nexrad_image *image = nexrad_radial_create_unprojected_image(radial, table, &radar, spheroid, 0.00815);
 
                     return image;
                 }
@@ -51,8 +69,6 @@ static nexrad_image *get_product_image(const char *file, nexrad_color_table *tab
                     nexrad_raster *raster = nexrad_raster_packet_open((nexrad_raster_packet *)packet);
 
                     nexrad_image *image = nexrad_raster_create_image(raster, table);
-
-                    fprintf(stderr, "Found raster packet, creating image\n");
 
                     return image;
                 }
@@ -72,6 +88,8 @@ static nexrad_image *get_product_image(const char *file, nexrad_color_table *tab
 
 error_symbology_block_open:
 error_message_get_symbology_block:
+error_message_read_station_location:
+error_geo_spheroid_create:
     nexrad_message_close(message);
 
 error_message_open:
