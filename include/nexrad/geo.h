913f62ac841a55485a2ff17d1575f47ae9f47a2c
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2013-10-31 11:37:13 -0500

    Initial implementation of radial map projection goo

--- include/nexrad/geo.h
+++ include/nexrad/geo.h
@@ -4,16 +4,19 @@
 #define NEXRAD_GEO_SPHEROID_CIRCUMFERENCE 6378137.0
 #define NEXRAD_GEO_SPHEROID_FLATTENING    (1/298.257223563)
 
-#define NEXRAD_GEO_NM_METERS 1852
+#define NEXRAD_GEO_NM_METERS       1852
+#define NEXRAD_GEO_COORD_MAGNITUDE    0.001
 
-#define NEXRAD_GEO_PROJECTION_MAGIC   "PROJ"
-#define NEXRAD_GEO_PROJECTION_VERSION 0x01
+#define NEXRAD_GEO_RADIAL_MAP_MAGIC   "PROJ"
+#define NEXRAD_GEO_RADIAL_MAP_VERSION 0x01
 
-enum nexrad_geo_projection_type {
-    NEXRAD_GEO_PROJECTION_NONE,
-    NEXRAD_GEO_PROJECTION_SPHEROID,
-    NEXRAD_GEO_PROJECTION_EQUIRECT,
-    NEXRAD_GEO_PROJECTION_MERCATOR
+#include <stdint.h>
+
+enum nexrad_geo_radial_map_type {
+    NEXRAD_GEO_RADIAL_MAP_NONE,
+    NEXRAD_GEO_RADIAL_MAP_SPHEROID,
+    NEXRAD_GEO_RADIAL_MAP_EQUIRECT,
+    NEXRAD_GEO_RADIAL_MAP_MERCATOR
 };
 
 typedef struct _nexrad_geo_polar {
@@ -26,7 +29,7 @@ typedef struct _nexrad_geo_cartesian {
     double lon;
 } nexrad_geo_cartesian;
 
-typedef struct _nexrad_geo_projection_header {
+typedef struct _nexrad_geo_radial_map_header {
     char     magic[4];
     uint16_t version;
     uint16_t type;
@@ -41,13 +44,21 @@ typedef struct _nexrad_geo_projection_header {
      * NEXRAD_GEO_PROJECTION_EQUIRECT: Scale of pixels in units of 0.001 degrees
      */
     uint32_t misc; /* Type-dependent value */
-    char     station_name[4];
+
+    /*
+     * Information pertaining to radar at center of map
+     */
      int32_t station_lat;
      int32_t station_lon;
-} nexrad_geo_projection_header;
+} nexrad_geo_radial_map_header;
+
+typedef struct _nexrad_geo_radial_map_point {
+    uint16_t azimuth;
+    uint16_t range;
+} nexrad_geo_radial_map_point;
 
 typedef struct _nexrad_geo_spheroid   nexrad_geo_spheroid;
-typedef struct _nexrad_geo_projection nexrad_geo_projection;
+typedef struct _nexrad_geo_radial_map nexrad_geo_radial_map;
 
 nexrad_geo_spheroid *nexrad_geo_spheroid_create();
 
@@ -68,5 +79,31 @@ void nexrad_geo_find_cartesian_dest(nexrad_geo_spheroid *spheroid,
 );
 
 void nexrad_geo_spheroid_destroy(nexrad_geo_spheroid *spheroid);
+
+void nexrad_geo_radial_map_find_extents(
+    nexrad_geo_spheroid *spheroid,
+    nexrad_geo_cartesian *radar,
+    uint16_t rangebins,
+    uint16_t rangebin_meters,
+    nexrad_geo_cartesian *extents
+);
+
+void nexrad_geo_radial_map_get_dimensions(
+    nexrad_geo_cartesian *extents,
+    double scale,
+    uint16_t *width,
+    uint16_t *height
+);
+
+nexrad_geo_radial_map *nexrad_geo_radial_map_create(
+    const char *path,
+    nexrad_geo_spheroid *spheroid,
+    nexrad_geo_cartesian *radar,
+    uint16_t rangebins,
+    uint16_t rangebin_meters,
+    double scale
+);
+
+void nexrad_geo_radial_map_close(nexrad_geo_radial_map *map);
 
 #endif /* _NEXRAD_GEO_POINT_H */
--- src/geo.c
+++ src/geo.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
-#include <stdint.h>
+#include <string.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <math.h>
-#include "geodesic.h"
 #include <errno.h>
+#include "geodesic.h"
 
 #include <nexrad/geo.h>
 
@@ -14,18 +18,14 @@ struct _nexrad_geo_spheroid {
     double flattening;
 };
 
-struct _nexrad_geo_projection {
+struct _nexrad_geo_radial_map {
     size_t size;
     size_t page_size;
     size_t mapped_size;
     int    fd;
 
-    nexrad_geo_projection_header * header;
-
-    struct {
-        uint16_t azimuth;
-        uint16_t range;
-    } * mappings;
+    nexrad_geo_radial_map_header * header;
+    nexrad_geo_radial_map_point  * points;
 };
 
 nexrad_geo_spheroid *nexrad_geo_spheroid_create() {
@@ -105,4 +105,102 @@ void nexrad_geo_spheroid_destroy(nexrad_geo_spheroid *spheroid) {
     }
 
     free(spheroid);
+}
+
+void nexrad_geo_radial_map_find_extents(nexrad_geo_spheroid *spheroid, nexrad_geo_cartesian *radar, uint16_t rangebins, uint16_t rangebin_meters, nexrad_geo_cartesian *extents) {
+    double range = rangebins * rangebin_meters;
+
+    uint16_t i, a;
+
+    for (i=0, a=0; i<4; i++, a+=90) {
+        nexrad_geo_polar polar = { a, range };
+
+        nexrad_geo_find_cartesian_dest(spheroid, radar, &extents[i], &polar);
+    }
+}
+
+void nexrad_geo_radial_map_get_dimensions(nexrad_geo_cartesian *extents, double scale, uint16_t *width, uint16_t *height) {
+    *width  = (uint16_t)round((extents[1].lon - extents[3].lon) / scale);
+    *height = (uint16_t)round((extents[0].lat - extents[2].lat) / scale);
+}
+
+static inline int _mapped_size(size_t size, size_t page_size) {
+    return size + (page_size - (size % page_size));
+}
+
+nexrad_geo_radial_map *nexrad_geo_radial_map_create(const char *path, nexrad_geo_spheroid *spheroid, nexrad_geo_cartesian *radar, uint16_t rangebins, uint16_t rangebin_meters, double scale) {
+    nexrad_geo_radial_map *map;
+    nexrad_geo_cartesian extents[4];
+    uint16_t width, height;
+
+    if (path == NULL || spheroid == NULL || radar == NULL) {
+        return NULL;
+    }
+
+    if ((map = malloc(sizeof(*map))) == NULL) {
+        goto error_malloc_map;
+    }
+
+    map->size = sizeof(nexrad_geo_radial_map_header)
+        + sizeof(nexrad_geo_radial_map_point) * width * height;
+
+    map->page_size   = (size_t)sysconf(_SC_PAGESIZE);
+    map->mapped_size = _mapped_size(map->size, map->page_size);
+
+    if ((map->fd = open(path, O_CREAT | O_RDWR, 0644)) < 0) {
+        goto error_open;
+    }
+
+    if ((map->header = mmap(NULL, map->mapped_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, map->fd, 0)) == NULL) {
+        goto error_mmap;
+    }
+
+    map->points = (nexrad_geo_radial_map_point *)map->header +
+        sizeof(nexrad_geo_radial_map_header);
+
+    nexrad_geo_radial_map_find_extents(
+        spheroid, radar, rangebins, rangebin_meters, extents
+    );
+
+    nexrad_geo_radial_map_get_dimensions(extents, scale, &width, &height);
+
+    memcpy(map->header->magic, NEXRAD_GEO_RADIAL_MAP_MAGIC, 4);
+
+    map->header->version         = htobe16(NEXRAD_GEO_RADIAL_MAP_VERSION);
+    map->header->type            = htobe16(NEXRAD_GEO_RADIAL_MAP_EQUIRECT);
+    map->header->width           = htobe16(width);
+    map->header->height          = htobe16(height);
+    map->header->rangebins       = htobe16(rangebins);
+    map->header->rangebin_meters = htobe16(rangebin_meters);
+    map->header->misc            = htobe32(scale / NEXRAD_GEO_COORD_MAGNITUDE);
+    map->header->station_lat     = htobe32(radar->lat / NEXRAD_GEO_COORD_MAGNITUDE);
+    map->header->station_lon     = htobe32(radar->lon / NEXRAD_GEO_COORD_MAGNITUDE);
+
+    memset(map->points, '\0', sizeof(nexrad_geo_radial_map_point) * width * height);
+
+    return map;
+
+error_mmap:
+    close(map->fd);
+
+error_open:
+    free(map);
+
+error_malloc_map:
+    return NULL;
+}
+
+void nexrad_geo_radial_map_close(nexrad_geo_radial_map *map) {
+    if (map == NULL)
+        return;
+
+    if (map->header)
+        munmap(map->header, map->mapped_size);
+
+    if (map->fd)
+        close(map->fd);
+
+    memset(map, '\0', sizeof(*map));
+
+    free(map);
 }
