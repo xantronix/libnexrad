38bbb5720c11d929ff4bde6356e9984df7c5c016
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2013-11-11 14:33:23 -0600

    Make proj.c create both equirectangular and Mercator radial maps

--- examples/proj.c
+++ examples/proj.c
@@ -14,8 +14,20 @@ int main(int argc, char **argv) {
 
     spheroid = nexrad_geo_spheroid_create();
 
-    if ((map = nexrad_geo_radial_map_create_equirect("foo.proj", spheroid, &radar, 346, 1000, 0.00815)) == NULL) {
-        perror("nexrad_geo_radial_map_create()");
+    if ((map = nexrad_geo_radial_map_create_equirect("equirect.proj", spheroid, &radar, 346, 1000, 0.00815)) == NULL) {
+        perror("nexrad_geo_radial_map_create_equirect()");
+        exit(1);
+    }
+
+    if (nexrad_geo_radial_map_save(map) < 0) {
+        perror("nexrad_geo_radial_map_save()");
+        exit(1);
+    }
+
+    nexrad_geo_radial_map_close(map);
+
+    if ((map = nexrad_geo_radial_map_create_mercator("mercator.proj", spheroid, &radar, 346, 1000, 0.00815)) == NULL) {
+        perror("nexrad_geo_radial_map_create_mercator()");
         exit(1);
     }
 
