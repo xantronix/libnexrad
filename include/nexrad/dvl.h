2d3e31d62601a6e6952025a7be69a73423ed91e5
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2014-03-05 10:37:24 -0600

    Add documentation to nexrad/dvl.h, nexrad/eet.h

--- include/nexrad/dvl.h
+++ include/nexrad/dvl.h
@@ -10,8 +10,36 @@
 #define NEXRAD_DVL_LOG_SCALE    38.875
 #define NEXRAD_DVL_LOG_OFFSET   83.875
 
+/*!
+ * \file nexrad/dvl.h
+ * \brief Functions for accessing Digital (High Resolution) VIL data
+ *
+ * An interface providing routines for determining the validity of a DVL
+ * reading, as well as for converting DVL samples into kg/m^-2 quantities.
+ */
+
+/*!
+ * \defgroup dvl NEXRAD Level III Digital Vertically Integrated Liquid data access routines
+ */
+
+/*!
+ * \ingroup dvl
+ * \brief Determine if DVL sample is valid
+ * \param v 8-bit DVL sample from product file
+ * \return 1 if valid, 0 if invalid
+ *
+ * Determine the validity of a sample of digital VIL data.
+ */
 int nexrad_dvl_valid(uint8_t v);
 
+/*!
+ * \ingroup dvl
+ * \brief Determine DVL value in units of kg/m^-2
+ * \param v 8-bit DVL sample fro mproduct file
+ * \return Quantity of liquid in units of kg/m^-2
+ *
+ * Determine the average quantity of liquid in units of kg/m^-2.
+ */
 double nexrad_dvl_vil(uint8_t v);
 
 #endif /* _NEXRAD_DVL_H */
--- include/nexrad/eet.h
+++ include/nexrad/eet.h
@@ -8,10 +8,47 @@
 #define NEXRAD_EET_VALUE_MASK  0x7f
 #define NEXRAD_EET_TOPPED_FLAG (1 << 7)
 
+/*!
+ * \file nexrad/eet.h
+ * \brief NEXRAD Level III Enhanced Echo Tops product data access routines
+ *
+ * Provides routines for accessing and converting raw Enhanced Echo Top data
+ * values to data in units of meters above mean sea level.
+ */
+
+/*!
+ * \defgroup eet NEXRAD Level III Enhanced Echo Tops data access routines
+ */
+
+/*!
+ * \ingroup eet
+ * \brief Determine if given rangebin value contains valid data within range
+ * \param v 8-bit data value from product file
+ * \return 1 if valid, 0 if invalid
+ *
+ * Determine if a rangebin sample contains valid data.
+ */
 int nexrad_eet_valid(uint8_t v);
 
+/*!
+ * \ingroup eet
+ * \brief Determine if given rangebin value reaches top of storm
+ * \param v 8-bit data value from product file
+ * \return 1 if topped, 0 if not topped
+ *
+ * Determine if a rangebin sample reaches the top of a storm.
+ */
 int nexrad_eet_topped(uint8_t v);
 
+/*!
+ * \ingroup eet
+ * \brief Determine maximum height of echo from raw data sample
+ * \param v 8-bit data value from product file
+ * \return Number of meters above mean sea level
+ *
+ * Determine the height of echoes returned based on a given Enhanced Echo Tops
+ * data sample, in meters above mean sea level.
+ */
 double nexrad_eet_meters(uint8_t v);
 
 #endif /* _NEXRAD_EET_H */
