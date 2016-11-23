2d4c6bba58efaab18123fe0fb9315ae81822df69
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2016-11-22 22:02:22 -0600

    Add byte swapping macros for additional C types

--- src/util.h
+++ src/util.h
@@ -12,12 +12,31 @@
      ((v & 0x0000ff00) <<  8) | \
      ((v & 0x000000ff) << 24))
 
+#define bswap64(v) \
+    (((v & 0xff00000000000000) >> 56) | \
+     ((v & 0x00ff000000000000) >> 40) | \
+     ((v & 0x0000ff0000000000) >> 16) | \
+     ((v & 0x000000ff00000000) >>  8) | \
+     ((v & 0x00000000ff000000) <<  8) | \
+     ((v & 0x0000000000ff0000) << 16) | \
+     ((v & 0x000000000000ff00) << 40) | \
+     ((v & 0x00000000000000ff) << 64))
+
 #ifndef _ENDIAN_H
 #ifdef __DO_SWAP_BYTES
 #define be16toh(v) ((uint16_t)bswap16((uint16_t)v))
 #define be32toh(v) ((uint32_t)bswap32((uint32_t)v))
 #define htobe16(v) ((uint16_t)bswap16((uint16_t)v))
 #define htobe32(v) ((uint32_t)bswap32((uint32_t)v))
+#define htole32(v) ((uint32_t)v)
+#define htole64(v) ((uint64_t)v)
+#else
+#define be16toh(v) ((uint16_t)v)
+#define be32toh(v) ((uint32_t)v)
+#define htobe16(v) ((uint16_t)v)
+#define htobe32(v) ((uint32_t)v)
+#define htole32(v) ((uint32_t)bswap32((uint32_t)v))
+#define htole64(v) ((uint64_t)bswap64((uint64_t)v))
 #endif /* __DO_SWAP_BYTES */
 #endif /* !_ENDIAN_H */
 
