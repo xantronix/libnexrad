a38429379f2fe1e0700739ed56c5dab9f6fb58b1
Alexandra Hrefna Hilmisdóttir <xan@xan.email>
2017-04-27 03:20:10 -0500

    Eliminate more math library calls; default to compiler typecast behavior for float->int

--- src/map.c
+++ src/map.c
@@ -165,8 +165,8 @@ nexrad_image *nexrad_map_project_radial(nexrad_radial *radial,
             while (heading.azimuth >= 360.0) heading.azimuth -= 360.0;
             while (heading.azimuth <    0.0) heading.azimuth += 360.0;
 
-            a = (uint16_t)abs((int)heading.azimuth * 10);
-            r = (uint16_t)roundf((heading.range * factor) / resolution);
+            a = (uint16_t)heading.azimuth * 10;
+            r = (uint16_t)((heading.range * factor) / resolution);
 
             if (r >= radial->bins)
                 continue;
